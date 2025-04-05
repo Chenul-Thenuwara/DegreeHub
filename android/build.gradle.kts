95050f853e4a0531f78bda6ac4b38e1eb8086641
Chenul Dulmika Thenuwara <chenuldulmikathenuwara@gmail.com>
2025-04-06 02:33:36 +0530

    feat: configure splash screen with light and dark mode support for all platforms
    - Added flutter_native_splash configuration for Android, iOS, and Web
    - Included light and dark mode splash images and branding assets
    - Customized Android 12 splash screen separately for consistency

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,3 +19,8 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
