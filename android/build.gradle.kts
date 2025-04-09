38237268a4adc3cc387ec24875c0e9dd9399bd41
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2025-04-09 19:20:02 +0530

    chore: Add Google Services plugin to build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15") // Add the Google Services plugin
+    }
+}
+
 allprojects {
     repositories {
         google()
